f7710d187481e48bb133ea75018a1ec36d92be91
YousifDev <86126571+YousifDev@users.noreply.github.com>
2022-02-17 17:26:29 +0300

    Update seek.js

--- src/Commands/seek.js
+++ src/Commands/seek.js
@@ -9,7 +9,7 @@ module.exports = {
 		if (message.guild.me.voice.channel && message.member.voice.channelId !== message.guild.me.voice.channelId) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.error}-You're not in the same **VoiceChannel**!`).setColor("RANDOM")] });
 		const queue = client.distube.getQueue(message.guildId);
 		if (!queue) return await message.channel.send({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.warning}-There's no **queue** in the server!`).setColor("RANDOM")] })
-		queue.songs.filter((song, num) => num === 0).map((song, num) => {
+		queue.songs.filter((song, num) => num === 0).map(async (song, num) => {
 			if (args[0] >= song.duration || args[0] < 0) return await message.channel.send({ embeds: [new MessageEmbed().setDescription(`${client.config.emojis.warning} • You can seek Track between \`0\` and \`${song.duration}\``)] });
 			await client.distube.seek(message.guildId, Number(args[0]));
 			await message.channel.send({ embeds: [new MessageEmbed().setDescription(`${client.config.emojis.time} • Track has been **Seeked** to \`${args[0]}\`.`).setColor("RANDOM")] });
