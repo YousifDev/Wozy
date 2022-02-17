77d1365b23704e78ff70ea885cccbd8d6b441717
YousifDev <86126571+YousifDev@users.noreply.github.com>
2022-02-17 19:04:43 +0300

    Update ping.js

--- src/Commands/ping.js
+++ src/Commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
 	async execute(message, args, client) {
 		await message.channel.send({
 			embeds: [
-				new MessageEmbed().setTitle(`🏓 Global Latency Summary:`).setDescription(`
+				new MessageEmbed().setDescription(`🏓 __Global Latency Summary:__
 				Discord API Latency: \`...ms\`
 				Discord Request Latency: \`...ms\``).setFooter(`Shanks • Asked by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
 					.setTimestamp()
@@ -13,9 +13,9 @@ module.exports = {
 		}).then(async (msg) => {
 			await msg.edit({
 				embeds: [
-					new MessageEmbed().setTitle(`🏓 Global Latency Summary:`).setDescription(`
+					new MessageEmbed().setDescription(`🏓 __Global Latency Summary:__
 					Discord API Latency: \`${client.ws.ping}ms\`
-					Discord Request Latency: \`${message.createdTimestamp - msg.createdTimestamp}ms\``).setFooter(`Shanks • Asked by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+					Discord Request Latency: \`${msg.createdTimestamp - message.createdTimestamp}ms\``).setFooter(`Shanks • Asked by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
 						.setTimestamp()
 				]
 			})
