b668fe2dec01389ccea5ed010b65eea9adf0bce2
YousifDev <86126571+YousifDev@users.noreply.github.com>
2022-02-17 17:08:20 +0300

    Add files via upload

--- src/Commands/seek.js
+++ src/Commands/seek.js
@@ -7,7 +7,7 @@ module.exports = {
 		if (!args.join(" ")) return await message.channel.send({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.warning}-Missing **arguments**!\n**•** Type a valid **Time**.`).setColor("RANDOM")] })
 		if (!message.member.voice.channel) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.error}-You're not in a **VoiceChannel**!`).setColor("RANDOM")] })
 		if (message.guild.me.voice.channel && message.member.voice.channelId !== message.guild.me.voice.channelId) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.error}-You're not in the same **VoiceChannel**!`).setColor("RANDOM")] });
-		await client.distube.seek(message.guildId, args[0]);
+		await client.distube.seek(message.guildId, Number(args[0]));
 		await message.channel.send({ embeds: [new MessageEmbed().setDescription(`${client.config.emojis.time} • Track has been **Seeked** to \`${args[0]}\`.`)] })
 	}
 }
