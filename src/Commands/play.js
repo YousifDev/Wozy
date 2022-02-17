f9c4b466c8710af2738c4894237ffd4469641e5f
YousifDev <86126571+YousifDev@users.noreply.github.com>
2022-02-17 03:10:01 +0300

    Update play.js

--- src/Commands/play.js
+++ src/Commands/play.js
@@ -6,10 +6,7 @@ module.exports = {
 		if (!args.join(" ")) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.warning}-Missing **arguments**!\n**•** Provide a Ttile of a Song,\n**•** Provide a URL of a Song.`).setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true })).setColor("RANDOM")] });
 		if (!message.member.voice.channel) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.error}-You're not in a **VoiceChannel**!`).setColor("RANDOM")] })
 		if (message.guild.me.voice.channel && message.member.voice.channelId !== message.guild.me.voice.channelId) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.error}-You're not in the same **VoiceChannel**!`).setColor("RANDOM")] });
-
 		await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.waiting}-Searching **of** \`${args.join(" ")}\` .`).setColor("RANDOM")] });
-		// await client.distube.play(message.member.voice.channel, args.join(" "), { textChannel: message.channel });
-	const queue = client.player.queues.get(message.guildId);
-	queue.play()
+		await client.distube.play(message.member.voice.channel, args.join(" "), { textChannel: message.channel });
 	}
 }
