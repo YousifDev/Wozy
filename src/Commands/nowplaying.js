4018a40a3364045ee9557c1a29462af6e8c7641c
YousifDev <86126571+YousifDev@users.noreply.github.com>
2022-02-17 18:15:38 +0300

    Add files via upload

---
+++ src/Commands/nowplaying.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+	name: "nowplaying",
+	aliases: ["np"],
+	async execute(message, args, client) {
+		const queue = player.getQueue(message.guild.id);
+		if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+		const progress = queue.createProgressBar();
+		const timestamp = queue.getPlayerTimestamp();
+		if (timestamp.progress == 'Infinity') return message.channel.send(`Playing a live, no data to display 🎧`);
+		message.channel.send(`${progress} (**${timestamp.progress}**%)`);
+	}
+}
