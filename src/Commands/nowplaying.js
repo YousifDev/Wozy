f20df72ad551f2761a35965a56cb640261a13155
YousifDev <86126571+YousifDev@users.noreply.github.com>
2022-02-17 18:46:57 +0300

    Add files via upload

--- src/Commands/nowplaying.js
+++ src/Commands/nowplaying.js
@@ -3,11 +3,11 @@ module.exports = {
 	name: "nowplaying",
 	aliases: ["np"],
 	async execute(message, args, client) {
-		if (!message.member.voice.channel) return await message.reply(`> ${client.config.emojis.error}-You're not in a **VoiceChannel**!`)
-		if (message.guild.me.voice.channel && message.member.voice.channelId !== message.guild.me.voice.channelId) return await message.reply(`> ${client.config.emojis.error}-You're not in the same **VoiceChannel**!`);
+		if (!message.member.voice.channel) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.error}-You're not in a **VoiceChannel**!`).setColor("RANDOM")] })
+		if (message.guild.me.voice.channel && message.member.voice.channelId !== message.guild.me.voice.channelId) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.error}-You're not in the same **VoiceChannel**!`).setColor("RANDOM")] });
 		const queue = client.distube.getQueue(message.guildId);
-		if (!queue) return await message.reply(`> ${client.config.emojis.warning}-There's no **queue** in the server!`);
-		if (!queue.playing) return await message.reply(`> ${client.config.emojis.warning}-There's no song **playing**!`);
+		if (!queue) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.warning}-There's no **queue** in the server!`).setColor("RANDOM")] });
+		if (!queue.playing) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.warning}-There's no song **playing**!`).setColor("RANDOM")] });
 		queue.songs.filter((song, num) => num < 1).map((song) => {
 			message.reply({
 				embeds: [
@@ -17,7 +17,7 @@ module.exports = {
 						.setImage(song.thumbnail)
 						.setColor("RANDOM")
 				]
-			})
+			});
 		});
 	}
 }
--- src/Commands/queue.js
+++ src/Commands/queue.js
@@ -3,11 +3,11 @@ module.exports = {
 	name: "queue",
 	aliases: ["القائمة"],
 	async execute(message, args, client) {
-		if (!message.member.voice.channel) return await message.reply(`> ${client.config.emojis.error}-You're not in a **VoiceChannel**!`)
-		if (message.guild.me.voice.channel && message.member.voice.channelId !== message.guild.me.voice.channelId) return await message.reply(`> ${client.config.emojis.error}-You're not in the same **VoiceChannel**!`);
+		if (!message.member.voice.channel) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.error}-You're not in a **VoiceChannel**!`).setColor("RANDOM")] })
+		if (message.guild.me.voice.channel && message.member.voice.channelId !== message.guild.me.voice.channelId) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.error}-You're not in the same **VoiceChannel**!`).setColor("RANDOM")] });
 		const queue = client.distube.getQueue(message.guildId);
-		if (!queue) return await message.reply(`> ${client.config.emojis.warning}-There's no **queue** in the server!`);
-		if (queue.songs.length <= 0) return await message.reply(`> ${client.config.emojis.warning}-There's no more **Songs**!`);
+		if (!queue) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.warning}-There's no **queue** in the server!`).setColor("RANDOM")] });
+		if (queue.songs.length <= 0) return await message.reply({ embeds: [new MessageEmbed().setDescription(`> ${client.config.emojis.warning}-There's no more **Songs**!`).setColor("RANDOM")] });
 		const queueList = queue.songs.filter((song, id) => id < 10).map((song, id) => `> \`${id + 1}\` :: __[${song.name}](${song.url})__ Duration [${song.formattedDuration}](${song.url}) ..`).join("\n")
 		if (queue.songs.length <= 10) {
 			await message.reply({
