10511c31ea84016c6ff0a1c8d3b9c3d827889aab
YousifDev <86126571+YousifDev@users.noreply.github.com>
2022-02-18 21:11:44 +0300

    Add files via upload

--- src/Events/message.js
+++ src/Events/message.js
@@ -1,3 +1,4 @@
+const { VoiceChannel } = require("../config.json");
 module.exports = {
 	name: "messageCreate",
 	async execute(message, client) {
@@ -5,11 +6,18 @@ module.exports = {
 		const args = message.content.trim().split(/ +/);
 		const cmdName = args.shift().toLowerCase();
 		const cmd = client.commands.get(cmdName) || client.aliases.get(cmdName);
-		if(!cmd) return;
-		try {
-			cmd.execute(message, args, client);
-		} catch(err) { 
-			console.log(err);
+		if (!message.member.voice.channel ||
+			message.member.voice.channel && message.member.voice.channel.id !== VoiceChannel ||
+			message.guild.me.voice.channel || message.guild.me.voice.channel &&
+			message.guild.me.voice.channel.id === VoiceChannel) {
+			return;
+		} else {
+			if (!cmd) return;
+			try {
+				cmd.execute(message, args, client);
+			} catch (err) {
+				console.log(err);
+			}
 		}
 	}
 }
--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,6 +1,16 @@
+const { joinVoiceChannel } = require("@discordjs/voice");
+const { VoiceChannel } = require("../config.json");
 module.exports = {
 	name: "ready",
 	async execute(c, client) {
 		console.log(`Logged in as : ${client.user.username} ""`);
+		const guild = client.guilds.cache.get("836791390100258897");
+		setInterval(async () => {
+			await joinVoiceChannel({
+				channelId: VoiceChannel,
+				guildId: "836791390100258897",
+				adapterCreator: guild.voiceAdapterCreator
+			})
+		}, 60000 * 30);
 	}
 }
