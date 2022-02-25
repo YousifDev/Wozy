945dcf63de6afd31a39159a9d2fbfbca57464e18
YousifDev <86126571+YousifDev@users.noreply.github.com>
2022-02-25 13:29:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -49,5 +49,6 @@ client.config = require("./config");
 require("./eventsHandler")(client);
 require("./commandsHandler")(client);
 require("./musicEvents")(client, distube);
+require("./Events/error")();
 
 client.login(Token);
